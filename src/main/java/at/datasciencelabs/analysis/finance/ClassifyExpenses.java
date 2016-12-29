b3ec9e45378c63c6bd8a7d1148d3911ee2a0c73d
phil3k <philip.limbeck@gmail.com>
2016-12-29 15:09:57 +0100

    experimented with adding another feature based on the expense amount

--- src/main/java/at/datasciencelabs/analysis/finance/ClassifyExpenses.java
+++ src/main/java/at/datasciencelabs/analysis/finance/ClassifyExpenses.java
@@ -2,20 +2,27 @@ package at.datasciencelabs.analysis.finance;
 
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.PairFunction;
-import org.apache.spark.ml.feature.HashingTF;
-import org.apache.spark.ml.feature.IDF;
-import org.apache.spark.ml.feature.IDFModel;
-import org.apache.spark.ml.feature.Tokenizer;
+import org.apache.spark.ml.feature.*;
 import org.apache.spark.ml.linalg.SparseVector;
+import org.apache.spark.ml.linalg.Vector;
+import org.apache.spark.ml.linalg.VectorUDT;
+import org.apache.spark.ml.linalg.Vectors;
 import org.apache.spark.mllib.classification.NaiveBayes;
 import org.apache.spark.mllib.classification.NaiveBayesModel;
 import org.apache.spark.mllib.regression.LabeledPoint;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.api.java.UDF1;
+import org.apache.spark.sql.types.DataTypes;
+import static org.apache.spark.sql.functions.*;
 import scala.Tuple2;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class ClassifyExpenses {
 
@@ -47,17 +54,67 @@ public class ClassifyExpenses {
 
         Dataset<Row> features2 = idfModel1.transform(features);
 
-        features2.printSchema();
-        JavaRDD<LabeledPoint> labeledPointJavaRDD = features2.javaRDD().map((Function<Row, LabeledPoint>) v1 -> {
+        sparkSession.udf().register("fromGermanDoubleString", new UDF1<String, Double>() {
+            @Override
+            public Double call(String s) throws Exception {
+                NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.GERMAN);
+                return numberFormat.parse(s).doubleValue();
+            }
+        }, DataTypes.DoubleType);
+
+        sparkSession.udf().register("vectorFromColumn", new UDF1<Double, Vector>() {
+            @Override
+            public Vector call(Double aDouble) throws Exception {
+                return Vectors.dense(aDouble);
+            }
+        }, new VectorUDT());
+
+        Dataset<Row> features2WithDataTypeChanged = features2.withColumn("amountDouble", callUDF("fromGermanDoubleString", features2.col("amount")));
+        features2WithDataTypeChanged.printSchema();
+
+        for(Row row : features2WithDataTypeChanged.takeAsList(5)) {
+            Double doubleValue = row.getAs("amountDouble");
+            System.out.println(doubleValue);
+        }
+        Dataset<Row> dataSetOnlyExpenses = features2WithDataTypeChanged.filter(new FilterFunction<Row>() {
+            @Override
+            public boolean call(Row value) throws Exception {
+                Double amount = value.getAs("amountDouble");
+                return amount < 0;
+            }
+        });
+        Dataset<Row> dataSetOnlyExpenses2 = dataSetOnlyExpenses.withColumn("vectorizedAmount", callUDF("vectorFromColumn", dataSetOnlyExpenses.col("amountDouble")));
+
+        MinMaxScaler scaler = new MinMaxScaler().setInputCol("vectorizedAmount").setOutputCol("amountNormalized").setMin(0.0).setMax(1.0);
+
+        Dataset<Row> transform = scaler.fit(dataSetOnlyExpenses2).transform(dataSetOnlyExpenses2);
+        transform.show();
+
+        JavaRDD<LabeledPoint> labeledPointJavaRDDWithExpenses = transform.javaRDD().map((Function<Row, LabeledPoint>) v1 -> {
+
+            Vector amount = v1.getAs("amountNormalized");
+
             SparseVector featureVector = v1.getAs(10);
+            int[] indices = featureVector.indices();
+            int[] newIndices = new int[indices.length+1];
+            System.arraycopy(indices, 0, newIndices, 0, indices.length);
+            newIndices[newIndices.length-1] = featureVector.size();
+
+            double[] values = featureVector.values();
+            double[] newValues = new double[values.length+1];
+            System.arraycopy(values, 0, newValues, 0, values.length);
+            newValues[newValues.length-1] = amount.toArray()[0];
+
             org.apache.spark.mllib.linalg.SparseVector newSparseVector = new org.apache.spark.mllib.linalg.SparseVector(
                     featureVector.size(),
                     featureVector.indices(),
                     featureVector.values());
+
             return new LabeledPoint((double)v1.getString(0).hashCode(), newSparseVector);
         });
 
-        JavaRDD<LabeledPoint>[] result = labeledPointJavaRDD.randomSplit(new double[]{0.6,0.4});
+
+        JavaRDD<LabeledPoint>[] result = labeledPointJavaRDDWithExpenses.randomSplit(new double[]{0.6,0.4}, 11L);
         final NaiveBayesModel model = NaiveBayes.train(result[0].rdd(), 1.0);
         JavaPairRDD<Double, Double> predictionAndLabel = result[1].mapToPair((PairFunction<LabeledPoint, Double, Double>) labeledPoint -> new Tuple2<>(model.predict(labeledPoint.features()), labeledPoint.label()));
 
